a4fcfb764389dff03d9c92239c8cca5029a95374
ABD EL RAOUF Ahmed <Ahmed.ABDELRAOUF@cegedim.com>
2020-12-06 13:46:46 +0200

    Adding propety module

---
+++ src/app/property/property.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PropertyComponent } from './property/property.component';
+
+
+
+@NgModule({
+  declarations: [PropertyComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PropertyModule { }
