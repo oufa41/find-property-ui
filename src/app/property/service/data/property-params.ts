7f3e71705e253c984b0c8e4918043a2a2fb3bfc9
ABD EL RAOUF Ahmed <Ahmed.ABDELRAOUF@cegedim.com>
2020-12-09 22:56:32 +0200

    Rename to PropertyParams

---
+++ src/app/property/service/data/property-params.ts
@@ -0,0 +1,13 @@
+export class PropertyParams {
+    address: string;
+    sellingType: string;
+
+    public constructor() {
+        this.address = '';
+        this.sellingType = '';
+    }
+    isAllEmpty(): boolean {
+        if (this.address === '' && this.sellingType === '') { return true; }
+        return false;
+    }
+}
